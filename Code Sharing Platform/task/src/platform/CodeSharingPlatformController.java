fc3579ac26778f1279ee022ec0967d821ddccce0
yadariya <66779337+yadariya@users.noreply.github.com>
2021-06-17 13:00:39 +0300

    program returns the same piece of code via the API and the web interface

--- Code Sharing Platform/task/src/platform/CodeSharingPlatformController.java
+++ Code Sharing Platform/task/src/platform/CodeSharingPlatformController.java
@@ -1,12 +1,20 @@
 package platform;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
 @RestController
 public class CodeSharingPlatformController {
+
+    final String DATE_FORMATTER = "yyyy/MM/dd HH:mm:ss";
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMATTER);
+
+    String given_code = "main()";
+    String date_t = "2020/01/01 12:00:03";
 
     @GetMapping("/code")
     public String getCode() {
@@ -15,20 +23,44 @@ public class CodeSharingPlatformController {
                 "    <title>Code</title>\n" +
                 "</head>\n" +
                 "<body>\n" +
-                "<pre>\n" +
-                "public static void main(String[] args) {\n" +
-                "    SpringApplication.run(CodeSharingPlatform.class, args);\n" +
-                "}</pre>\n" +
+                "<span id=\"load_date\">\n" +
+                date_t +
+                "</span>\n" +
+                "<pre id=\"code_snippet\">\n" +
+                given_code +
+                "</pre>\n" +
                 "</body>\n" +
                 "</html>";
     }
 
     @GetMapping("/api/code")
+    @ResponseBody
     public HashMap<String, String> getCodeAPI() {
         HashMap<String, String> map = new HashMap<>();
-        map.put("code", "public static void main(String[] args) {\n" +
-                "    SpringApplication.run(CodeSharingPlatform.class, args);\n" +
-                "}");
+        map.put("code", given_code);
+        map.put("date", date_t);
         return map;
+    }
+
+    @GetMapping("/code/new")
+    public String getCodeNew() {
+        return "<!DOCTYPE html>\n" +
+                "<html lang=\"en\" xmlns:th=\"https://www.thymeleaf.org/\">\n" +
+                "<head>\n" +
+                "    <title>Create</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<textarea id=\"code_snippet\"> ... </textarea>\n" +
+                "<button id=\"send_snippet\" type=\"submit\" onclick=\"send()\">Submit</button>\n" +
+                "</body>\n" +
+                "</html>";
+    }
+
+    @PostMapping("/api/code/new")
+    public ResponseEntity<EmptyJsonBody> postCode(@RequestBody CodeJson codeJson) {
+        given_code = codeJson.getCode();
+        date_t = LocalDateTime.now().format(formatter);
+        ResponseEntity.BodyBuilder responseBuilder = ResponseEntity.ok();
+        return responseBuilder.body(new EmptyJsonBody());
     }
 }
